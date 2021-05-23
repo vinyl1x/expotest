65c4da89942d8ce7dbb5d912a689048d7922e4d6
niallotuathail <niallotuathail@gmail.com>
2021-05-23 13:48:21 +0100

    Included image as tab icon

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+function Header(){
+  return(
+    <Header
+      centerComponent = {{text: 'Prime For Life'}}
+    /> 
+  )
+}
+
 function CoachScreen() {
   return (
+
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Coaching Screen!</Text>
+      <Image source={require('./assets/logo.png')} style={styles.headerImage} />
+      <Text>Coaching Screen Here!</Text>
     </View>
   );
 }
@@ -70,7 +80,20 @@ export default function App() {
           inactiveTintColor: 'gray',
         }}
       >
-        <Tab.Screen name="Sasha" component={CoachScreen} />
+        <Tab.Screen name="Sasha" component={CoachScreen} 
+          options = {{
+            tabBarIcon:  () => (
+              <View>
+                <Image
+                  source = {require('./assets/sasha.jpeg')}
+                  style = {{
+                    width: 30,
+                    height: 30,
+                    borderRadius: 30/2,
+                  }}
+                  />
+              </View>
+            )}}/>
         <Tab.Screen name="Library" component={LibraryScreen} />
         <Tab.Screen name="Tracking" component={TrackingScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
@@ -78,3 +101,10 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  headerImage: {
+    width: 26,
+    height: 26,
+  }
+});
